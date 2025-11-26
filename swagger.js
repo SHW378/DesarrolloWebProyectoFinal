b784596b25046e30a010493f803af458c15f2a56
SHW378 <cesarbrmn@hotmail.com>
2025-11-25 23:34:05 -0600

    Estructura base del proyecto

---
+++ swagger.js
@@ -0,0 +1,33 @@
+//Swagger.js
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+//Configuración de Swagger
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Documentación de la API', //Titulo de la documentación
+    version: '1.0.0', //Version de la API
+    description: 'Documentacion de la API con Swagger'
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000', //URL base de la API
+      description: 'Servidor de desarrollo'
+    }
+  ]
+};
+
+const options = {
+  swaggerDefinition,
+  //Paths to files
+  apis: ['./routes/*.js'] //Ajustamos esto en la ruta de los archivos
+};
+
+const swaggerSpec = swaggerJSDoc(options)
+
+function setupSwagger(app){
+    app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+};
+
+module.exports = setupSwagger
